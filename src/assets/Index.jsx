a59fec30e2fd3746d6ad6b1bcacc8e679b526d6f
MashoorNEGI <ayushdeepnegi@gmail.com>
2023-09-08 19:32:16 +0530

    deploying mistake

--- src/assets/Index.jsx
+++ src/assets/Index.jsx
@@ -2,4 +2,4 @@ import { lazy } from "react";
 export const Home = lazy(() => import('../pages/Home'));
 export const Contact = lazy(() => import('../pages/Contact'));
 export const About = lazy(() => import('../pages/About'));
-export const Projects = lazy(() => import('../Pages/Projects.jsx'));
+export const Projects = lazy(() => import('../pages/Projects'));
