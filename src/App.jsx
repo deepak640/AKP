d2d2e77d53763451f33c5882aab8646b12191968
MashoorNEGI <ayushdeepnegi@gmail.com>
2023-09-08 18:24:26 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import Router from "./Routes/Router";
+// import Router from "./Routes/Router";
 import AOS from 'aos';
+import React, { Suspense } from 'react'
+import { Routes as Switch, Route, BrowserRouter } from 'react-router-dom';
+import Loader from "./assets/Loader";
+import { Home, About, Contact, Projects } from "./assets";
+
 import 'aos/dist/aos.css';
 function App() {
   const [count, setCount] = useState(0)
@@ -15,7 +20,17 @@ function App() {
 
   return (
     <>
-      <Router />
+      <BrowserRouter>
+        <Suspense fallback={<Loader />}>
+          <Switch>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/project' element={<Projects />} />
+          </Switch>
+        </Suspense>
+      </BrowserRouter>
+      {/* <Router /> */}
     </>
   )
 }
