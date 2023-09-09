04223f2b612e6a7ccb1e649f964d7611edb4676f
MashoorNEGI <ayushdeepnegi@gmail.com>
2023-09-09 09:44:02 +0530

    updated phone number

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,28 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from "../assets/Layout";
 import { useForm } from '@formspree/react';
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css'
 
 const Contact = () => {
     const [state, handleSubmit] = useForm("mbjvyblb");
+    const [value, setValue] = useState()
     if (state.succeeded) {
         return <p>Thanks for joining!</p>;
     }
     return (
         <Layout>
             <div className="contact-container">
-                <form className="contact-box" action='https://formspree.io/f/mbjvyblb' method='POST'>
+                <form className="contact-box" onSubmit={handleSubmit}>
                     <div className="left"></div>
                     <div className="right">
                         <h2>Contact Us</h2>
                         <input type="text" name='name' className="field" id="" placeholder='Enter Name' required />
                         <input type="email" name='email' className="field" id="" placeholder='Enter Email' required />
-                        <input type="Phone Number" name='phone number' className="field" id="" placeholder='Enter Phone number' required />
+                        {/* <input type="Phone Number" name='phone number' className="field" id="" placeholder='Enter Phone number' required /> */}
+                        <PhoneInput className='field'
+                            placeholder="Enter phone number"
+                            defaultCountry='IN'
+                            name='phone number'
+                            value={value}
+                            onChange={setValue} />
                         <textarea className="field area" name='message' id="" cols='20' placeholder='message' required></textarea>
                         <button className='btn'>SUBMIT</button>
                     </div>
                 </form>
-            </div>
-        </Layout>
+            </div >
+        </Layout >
 
     )
 }
